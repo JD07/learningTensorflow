32005066bed6a58d34a5fe8d22b291db405a676d
JD07 <1210691486@qq.com>
2018-11-22 20:53:27 +0800

    correct some error

--- ADCP/dataPrepare/splitData.m
+++ ADCP/dataPrepare/splitData.m
@@ -5,7 +5,7 @@ clear all;clc;
 %ÏÈ¶ÁÊı¾İ
 maxIter=3;
 A=[];B=[];
-for iter=11:maxIter+10
+for iter=1:maxIter
    load(['result',num2str(iter),'.mat']);
    load(['tag',num2str(iter),'.mat']);
    A=[A;elpsetMF];
@@ -20,11 +20,11 @@ B_new=B(randIndex,:);
 %%
 clear A B
 threshold=0.9;
-date=A_new(1:threshold*size(A_new,1),:);
-tag=A_new(threshold*size(A_new,1)+1:size(A_new,1),:);
-save("train_data2", 'date');
-save("train_tag2",'tag');
-date=B_new(1:threshold*size(B_new,1),:);
-tag=B_new(threshold*size(B_new,1)+1:size(B_new,1),:);
-save("test_data2",'date');
-save("test_tag2",'tag');
+data1=A_new(1:threshold*size(A_new,1),:);
+data2=A_new(threshold*size(A_new,1)+1:size(A_new,1),:);
+save("train_data1", 'data1');
+save("test_data1",'data2');
+tag1=B_new(1:threshold*size(B_new,1),:);
+tag2=B_new(threshold*size(B_new,1)+1:size(B_new,1),:);
+save("train_tag1",'tag1');
+save("test_tag1",'tag2');
